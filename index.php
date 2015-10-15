b62e0706e9331ed273abbf41c53a2938b2c842ad
gerald-fongwe <geraldjam2004@yahoo.com>
2015-10-15 09:05:56 -0600

    added amazon product page

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>My Amazon Product Page</title>
+	</head>
+	<body>
+		<h1>Shopper's Persona</h1>
+
+		<p>He has a level of income and responsibility similar in character to online shoppers. He knows what he wants
+			but does not have time to go out shopping and wants his product delivered to him at his residence. And also he
+			may want his product as soon as possible. He may decide to do some online research about product before making
+			a purchase. Generally in this case, he is in no hurry to get the product. He must have a laptop, Intel core i3
+			Windows 7 and has access to the Internet. He has a valid method of payment in order to make an online
+			transaction. He is educated enough to comfortably browse the internet. Continuous use of the online shopping
+			option, increases his experience and possibility of developing a confident attitudes towards new online
+			shopping ventures. He is generally the age of 25 and above.</p>
+
+		<h1>User Case</h1>
+		<h2>User Case 1</h2>
+		<ol>
+			<li>he must opens website</li>
+			<li>He may  or may not create a user account</li>
+			<li>Searches and Clicks on the product</li>
+			<li>He enters his personal and  credit card information</li>
+			<li>Submits the  information for the purchase to be processed and effective</>
+			<li>Waits for a confirmation number for purchase</li>
+			<li>Electronic purchase receipt</li>
+			10.   <li>He prints the receipt or store the e-copy in his system</li>
+		</ol>
+		<h2>User Case 2</h2>
+		<ol>
+			<li>Buyer must open the website</li>
+			<li>Buyer may  or may not create a user account</li>
+			<li>He  enters the name of product he is looking for  into the site web shopper search engine</li>
+			<li>He gets a number  of  products displayed from alternative vendors.</li>
+			<li>He  clicks on a brand and product and go through all the ones of interest to him</li>
+			<li>Searching and reading the reviews from previous shoppers</li>
+			<li>He goes through all the brand and products and finds one after careful considerations</li>
+			<li>He finds the product with a good rating that meets his expectations</li>
+			<li>He puts the selected product  in the carte</li>
+			<li>He checks out</li>
+			<li>Enters  his personal and credit card information</li>
+			<li>bmits information for purchase to be processed and effective</li>
+			<li>Waits for confirmation number  of purchase</li>
+			<li>Electronic purchase receipt</li>
+			<li>He prints the receipt or store the e-copy in his system</li>
+		</ol>
+	</body>
+</html>
